a69b0f58c96bb2a5217c179eaa3236397b02176a
Jason Zhou <friedchicken2go@gmail.com>
2020-04-16 18:11:33 -0700

    added mmeber class and updated UI

--- app/src/main/java/com/example/comp_3717_final_exam/MainActivity.java
+++ app/src/main/java/com/example/comp_3717_final_exam/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.comp_3717_final_exam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText titleEdit, descriptionEdit, linkEdit;
+    Button save, nextPage;
+    DatabaseReference reff;
+    Member member;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titleEdit = (EditText)findViewById(R.id.titleEdit);
+        descriptionEdit = (EditText)findViewById(R.id.descriptionEdit);
+        linkEdit = (EditText)findViewById(R.id.linkEdit);
+
+        save = (Button)findViewById(R.id.save);
+        nextPage = (Button)findViewById(R.id.nextPage);
+
+        member = new Member();
+
+        reff = FirebaseDatabase.getInstance().getReference().child("Movies");
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                String title = titleEdit.getText().toString();
+                String description = descriptionEdit.getText().toString();
+                String link = linkEdit.getText().toString();
+
+                member.setTitle(title);
+                member.setDescription(description);
+                member.setLink(link);
+                reff.child(title).setValue(member);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/comp_3717_final_exam/Member.java
@@ -0,0 +1,33 @@
+package com.example.comp_3717_final_exam;
+
+public class Member {
+
+    private String Title, Description, Link;
+
+    public Member(){
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getLink() {
+        return Link;
+    }
+
+    public void setLink(String link) {
+        Link = link;
+    }
+}
